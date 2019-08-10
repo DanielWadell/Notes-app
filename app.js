e68d3bf3bad1e618e93de7cac4afede50bc582c2
Daniel Wadell <danielwadell@daniels-mbp.home>
2019-08-10 13:48:11 -0700

    can now add notes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-
+const notes = require('./notes.js')
 // console.log(chalk.green.bold.underline('Success!!!'))
 
 
@@ -21,8 +21,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
